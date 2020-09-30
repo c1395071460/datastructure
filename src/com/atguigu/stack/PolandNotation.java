a2c1ef6ebfdf411e2b1b03febe9f552a58bb2564
c1395071460 <1395071460@qq.com>
2020-10-01 01:13:55 +0800

    中缀转后缀表达式 注释

--- src/com/atguigu/stack/PolandNotation.java
+++ src/com/atguigu/stack/PolandNotation.java
@@ -13,6 +13,19 @@ import java.util.Stack;
 public class PolandNotation {
 
     public static void main(String[] args) {
+
+        //完成将一个中缀表达式转成后缀表达式的功能
+        //说明
+        //1. 1 + ((2+3)*4) - 5 转成 1 2 3 + 4 * + 5 -
+        //2.因为直接对一个字符串进行操作 不方便  因此 现将 "1 + ((2+3)*4) - 5" =>中缀的表达式对应的List
+        //即 "1 + ((2+3)*4) - 5" =》 arrayList[1 ,+ ,(,(,2,+,3,),*,4,) ,- ,5]
+        //3. 将得到的中缀表式对应的List  =》 后缀表达式对应的List
+        //即 arrayList[1 ,+ ,(,(,2,+,3,),*,4,) ,- ,5] =》 ArrayList[1 2 3 + 4 * + 5 -]
+        String expression = "1+((2+3)*4)-5";
+        List<String> infixExpressionList = toInfixExpressionList(expression);
+        System.out.println(infixExpressionList);
+/*
+
         //先定义逆波兰表达式
         //(3+4)*5-6 => 3 4 + 5 * 6 -
         //说明为了方便 逆波兰表达式 的数字和符号使用空格隔开
@@ -23,7 +36,35 @@ public class PolandNotation {
         List<String> rpnList = getListString(suffixExpression);
         System.out.println(rpnList);
         int calculate = calculate(rpnList);
-        System.out.println("计算的结果是:" + calculate);
+        System.out.println("计算的结果是:" + calculate);*/
+    }
+
+    //将中缀表达式转成对应的List
+    // s = "1+((2+3)*4)-5"
+    public static List<String> toInfixExpressionList(String s) {
+        //定义一个List 存放中缀表达式对应的内容
+        List<String> ls = new ArrayList<>();
+        //一个指针 用于遍历中缀表达式字符串
+        int i = 0;
+        String str; //对多位数的拼接
+        char c; //每遍历到一个字符 就放到c中
+        do {
+            //如果c是一个非数字 我需要计入到ls
+            if ((c = s.charAt(i)) < 48 || (c = s.charAt(i)) > 57) {
+                ls.add("" + c);
+                i++;
+            } else { // 如果是数字 需要考虑多位数的问题
+                str = "";//现将str 置程空串
+                while (i < s.length() && (c = s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57) {
+                    str += c;//拼接
+                    i++;
+                }
+                ls.add(str);
+            }
+        } while (i < s.length());
+
+        return ls;
+
     }
 
     //将一个逆波兰表达式 一次将数据和运算符 放入到ArrayList中
